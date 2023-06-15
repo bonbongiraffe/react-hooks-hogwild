37a0aa4f0aa099f577e2b312c9f774dfbcb1a62f
bonbongiraffe <francescowai@yahoo.com>
2023-06-15 11:00:53 -0400

    wip2

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,11 +2,36 @@ import React, {useState} from "react"
 import HogTile from "./HogTile";
 
 function Body({ hogs }){
-
     const[greasedPigs, setGreasedPigs] = useState(false)
+    const[sortCriteria, setSortCriteria] = useState("all")
+    //filter handler
     function handleClick(){
         setGreasedPigs(!greasedPigs)
     }
+    //sort handler
+    function handleChange(event){
+        setSortCriteria(sortCriteria = event.target.value)
+        if (sortCriteria === "weight")
+            hogs = sortByWeight(hogs)
+        else if (sortCriteria === "name")
+            hogs = sortByName(hogs)           
+    }
+    //sort functions
+    function sortByWeight(hogs){
+        return hogs.sort((a,b) => {return a.weight - b.weight})
+    }
+    function sortByName(hogs){
+        return hogs.sort((a,b) => {
+            let fa = a.name.toLowerCase()
+            let fb = b.name.toLowerCase()
+            if (fa < fb)
+                return -1
+            if (fa > fb)
+                return +1
+            return 0
+        })
+    }
+    //filter operations
     const filteredHogs = greasedPigs ? hogs.filter((hog) => hog.greased === true ) : hogs
     const hogTiles = filteredHogs.map((hog) => <HogTile
         name={hog.name}
@@ -18,12 +43,22 @@ function Body({ hogs }){
         key={hog.name}/>)
     
     return (
-       <body>
+        <React.Fragment>
+            {/* factor out search-bar into its own component if there's time */}
+        <div class="search-bar"> 
             <button onClick={handleClick}>{greasedPigs ? "Show All Pigs" : "Show Greased Pigs"}</button>
+            <select onChange={(event) => handleChange(event)}>
+                <option value="all">Sort by...</option>
+                <option value="name">Sort by Name</option>
+                <option value="weight">Sort by Weight</option>
+            </select>
+        </div>
+        <body>
             <div className="ui grid container">
                 {hogTiles}
             </div>
-       </body>
+        </body>
+        </React.Fragment>
     );
 }
 
